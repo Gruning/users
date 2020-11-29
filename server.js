1e270c923181db102bb3134d181e50d78826f537
Alberto Gruning <kimono-mac@Albertos-MacBook-Air.local>
2020-11-29 18:34:17 -0300

    cap7-registering

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const ExpressGraphQL = require('express-graphql')
 
 const app = express()
+
+app.use('/graphql',ExpressGraphQL({
+  graphql: true  
+}))
 
 app.listen(4000,()=>{
   console.log('listening')
